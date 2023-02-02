1d2df1794a82fbb44efbb363f5801f7ae4b627b1
Its-notbad <chamber718@gmail.com>
2023-02-02 20:02:18 +0900

    1일차 Scanner

---
+++ Study/src/Study1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Study1 {
+    public static void main(String[] args) {
+        System.out.println("첫번째날");
+
+        String number = "123";
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("    입력해 : ");
+
+        //nextint : 숫자, next 문자
+        //int input = keyboard.nextInt();
+        String input = keyboard.next();
+
+        if (input.equals(number)){
+            System.out.print("일치");
+        } else {
+            System.out.print("불일치");
+
+        }
+
+        keyboard.close();
+    }
+}
